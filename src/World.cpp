f91c3e871837b79847e18f408836c00c91e7aff7
robinfaury <robinfaury@gmail.com>
2015-03-23 14:18:40 +0100

    change shader path

--- src/World.cpp
+++ src/World.cpp
@@ -6,7 +6,7 @@ World::World()
 
 void World::createScene()
 {
-	this->listOfShader.push_back(new Shader("../IN55-Project/res/shader/basic.vert", "res/shader/basic.frag"));
+	this->listOfShader.push_back(new Shader("../IN55-Project/res/shader/basic.vert", "../IN55-Project/res/shader/basic.frag"));
 	this->listOfMesh.push_back(new Mesh("../IN55-Project/res/cube_n.obj", this->listOfShader.at(0)));
 	//this->listOfMesh.push_back(new Mesh("res/Big_Ben.obj", this->listOfShader.at(0)));
 }
