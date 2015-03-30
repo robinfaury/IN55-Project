9d32c887aa580bb33aded15185e1f7bd66582d4a
Milan "Amarre" Irigoyen <edrevan@gmail.com>
2015-03-30 10:49:11 +0200

    Observable model, V2

--- src/Observable.cpp
+++ src/Observable.cpp
@@ -9,17 +9,3 @@ Observable::Observable()
 Observable::~Observable()
 {
 }
-
-void Observable::AddObs(Observer* obs)
-{
-	m_list.push_back(obs);
-}
-
-void Observable::DelObs(Observer* obs)
-{
-	for (list<Observer*>::iterator it = m_list.begin(); it != m_list.end(); ++it)
-	{
-		if (obs == *it)
-			it = m_list.erase(it);
-	}
-}
--- src/Observable.h
+++ src/Observable.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <list>
+#include <string>
+#include <set>
 #include "Observer"
 
 using namespace std;
@@ -10,18 +11,26 @@ typedef int Info;
 class Observable
 {
 private:
-	std::list<Observer*> m_list;
-
-	typedef std::list<Observer*>::iterator iterator;
-	typedef std::list<Observer*>::const_iterator const_iterator;
+	set<IObserver*> list_observers;
 
 public:
-	void AddObs(Observer* obs);
-	void DelObs(Observer* obs);
+	void notify(string data) const
+	{
+		// Notifier tous les observers
+		for (set<IObserver*>::const_iterator it = list_observers.begin();
+			it != list_observers.end(); ++it)
+			(*it)->update(data);
+	}
 
-	virtual Info Statut(void) const = 0;
-	virtual ~Observable();
-protected:
-	void Notify(void);
+	void addObserver(IObserver* observer)
+	{
+		// Ajouter un observer a la liste
+		list_observers.insert(observer);
+	}
 
+	void removeObserver(IObserver* observer)
+	{
+		// Enlever un observer a la liste
+		list_observers.erase(observer);
+	}
 };
